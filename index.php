1129f093c52e4c8c348dd12dc53489a2f56d78d7
Dicky Prasetya Widi <94689361+Dickyprst@users.noreply.github.com>
2022-05-18 20:08:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+<html>
+
+<head>
+
+            <title>Kalkulator Sederhana</title>
+
+            <link rel="stylesheet" type="text/css" href="<?= base_url('assets/style.css')?>">
+
+</head>
+
+<body>
+
+            <div class="kalkulator">
+
+                        <h2 class="judul">KALKULATOR</h2>
+
+                        <form method="post" action="">                              
+
+                                    <input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama" required="required">
+
+                                    <input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua" required="required">
+
+                                    <select class="opt" name="operasi">
+
+                                                <option value="tambah">+</option>
+
+                                                <option value="kali">x</option>
+
+                                    </select>
+
+                                    <input type="submit" name="hitung" value="Hitung" class="tombol">                               </form>
+
+                        <input type="text" value="<?php echo $oper; ?>" class="bil">
+
+            </div>
+
+</body>
+
+</html>
+
+class Kalkulator{
+
+            public function tambah($bilangan1, $bilangan2){
+
+        $tambah = $bilangan1 + $bilangan2;
+
+            }
+
+    public function kurang($bilangan1, $bilangan2){
+
+        $kurang = $bilangan1 - $bilangan2;
+
+    }
+
+    public function kali($bilangan1, $bilangan2){
+
+        $kali = $bilangan1 * $bilangan2;
+
+    }
+
+    public function bagi($bilangan1, $bilangan2){
+
+        $bagi = $bilangan1 / $bilangan2;
+
+            }
+
+}
+?>
